b4c503fc162980b8b5516456188dd991175c0e23
ErikHartman <erik.hartman@hotmail.com>
2025-02-24 14:55:13 +0100

    Update stub for benchmarking. Currently crashes when reaching some pdb

--- src/scoring_benchmark/run_scoring_benchmark.py
+++ src/scoring_benchmark/run_scoring_benchmark.py
@@ -1,20 +1,37 @@
 import os
 from bopep import Scorer
+import pandas as pd
+from multiprocessing import Pool
+
+def process_pdb(pdb_path):
+    scorer = Scorer()
+    print(pdb_path)
+    try:
+        pdb_scores = scorer.calculate_rosetta_scores(pdb_path)
+        pdb_scores['pdb'] = os.path.basename(pdb_path)
+        return pdb_scores
+    except:
+        return None
 
 if __name__ == "__main__":
-
+    """
+    TODO: Figure out why this breaks for certain pdbs.
+    """
     data_dir = os.path.abspath(
         "/srv/data1/general/immunopeptides_data/databases/benchmark_data"
     )
-
     pdb_dir = os.path.join(data_dir, "pdbs")
+    
+    pdb_paths = [os.path.join(pdb_dir, pdb) for pdb in os.listdir(pdb_dir)]
+    
+    num_processes = 72
+    
+    with Pool(processes=num_processes) as pool:
+        all_scores = pool.map(process_pdb, pdb_paths)
+    
+    all_scores = [score for score in all_scores if score is not None]
+    scores_df = pd.DataFrame(all_scores)
+    scores_df.set_index('pdb', inplace=True)
 
-    scorer = Scorer()
-
-    for pdb in os.listdir(pdb_dir):
-        pdb_path = os.path.join(pdb_dir, pdb)
-        print(pdb_path)
-        print(
-            scorer.calculate_rosetta_scores(pdb_path),
-        )
-        break
+    output_dir = os.path.join(data_dir, "scores.csv")
+    scores_df.to_csv(output_dir)
