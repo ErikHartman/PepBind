b3937ced7bcd89acf20009a7471d858a9982f41c
Malcom Siljehag Alencar <malcolm@alencar.se>
2025-03-04 12:22:48 +0100

    Removed some logging to make code clearer

--- src/scoring_benchmark/run_scoring_benchmark.py
+++ src/scoring_benchmark/run_scoring_benchmark.py
@@ -28,43 +28,43 @@ def process_pdb(pdb_path):
 # Main file to run scoring benchmark
 
 
-# if __name__ == "__main__":
-#     """
-#     TODO: Figure out why this breaks for certain pdbs.
-#     - Reason 1: Cyclical peptides
-#     - Read
-#     """
-#     data_dir = os.path.abspath(
-#         "/srv/data1/general/immunopeptides_data/databases/benchmark_data"
-#     )
-#     pdb_dir = os.path.join(data_dir, "pdbs")
-    
-#     pdb_paths = [os.path.join(pdb_dir, pdb) for pdb in os.listdir(pdb_dir)]
-    
-#     num_processes = 36
-    
-#     with Pool(processes=num_processes) as pool:
-#         all_scores = pool.map(process_pdb, pdb_paths)
-    
-#     all_scores = [score for score in all_scores if score is not None]
-#     scores_df = pd.DataFrame(all_scores)
-#     scores_df.set_index('pdb', inplace=True)
 
-#     output_dir = os.path.join(data_dir, "scores.csv")
-#     scores_df.to_csv(output_dir)
+if __name__ == "__main__":
+    """
+    TODO: Figure out why this breaks for certain pdbs.
+    """
+    data_dir = os.path.abspath(
+        "/srv/data1/general/immunopeptides_data/databases/benchmark_data"
+    )
 
+    pdb_dir = os.path.join(data_dir, "pdbs")
+    
+    pdb_paths = [os.path.join(pdb_dir, pdb) for pdb in os.listdir(pdb_dir)]
+    
+    num_processes = 20
+    
+    with Pool(processes=num_processes) as pool:
+        all_scores = pool.map(process_pdb, pdb_paths)
+    
+    all_scores = [score for score in all_scores if score is not None]
+    scores_df = pd.DataFrame(all_scores)
+    scores_df.set_index('pdb', inplace=True)
+
+    output_dir = os.path.join(data_dir, "scores.csv")
+    scores_df.to_csv(output_dir)
 
 
 # Single file processing, to test the process_pdb function which is currently not working
 
-data_dir = os.path.abspath(
-        "/srv/data1/general/immunopeptides_data/databases/benchmark_data"
-    )
-single_pdb_path = "/srv/data1/general/immunopeptides_data/databases/benchmark_data/pdbs/6gd5.pdb"
-single_score = process_pdb(single_pdb_path)
+# if __name__ == "__main__":
+#     data_dir = os.path.abspath(
+#         "/srv/data1/general/immunopeptides_data/databases/benchmark_data"
+#     )
+#     pdb_dir = os.path.join(data_dir, "pdbs/6gd5.pdb")
+#     single_score = process_pdb(pdb_dir)
 
-if single_score is not None:
-    single_score_df = pd.DataFrame([single_score])
-    single_score_df.set_index('pdb', inplace=True)
-    single_output_path = os.path.join(data_dir, "single_score.csv")
-    single_score_df.to_csv(single_output_path)
+#     if single_score is not None:
+#         single_score_df = pd.DataFrame([single_score])
+#         single_score_df.set_index('pdb', inplace=True)
+#         single_output_path = os.path.join(data_dir, "single_score.csv")
+#         single_score_df.to_csv(single_output_path)
